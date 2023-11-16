7af959cc1f4e18ef7e6f03cd74c5cca39a1fa199
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-11-16 19:17:53 +0000

    basic servers

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+const PORT = 80;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
