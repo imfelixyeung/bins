5694152ee8fa19037fcdb539580830a590886040
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-08-15 22:49:38 +0100

    setup geist sans

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/lib/utils";
 import "./globals.css";
+import { GeistSans } from "geist/font/sans";
 
 export default async function RootLayout({
   children,
@@ -6,7 +8,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn(GeistSans.variable)}>
       <body>{children}</body>
     </html>
   );
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,13 +1,9 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   prefix: "",
   theme: {
     container: {
@@ -72,9 +68,12 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
