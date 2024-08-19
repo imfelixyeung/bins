9c5b5b2cf7ea504d7f7193b0764031c1887ce383
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-08-19 22:15:59 +0100

    sanitise before searching

--- apps/web/src/functions/search-premises.ts
+++ apps/web/src/functions/search-premises.ts
@@ -5,8 +5,10 @@ import { unstable_cache } from "next/cache";
 
 export const searchPremises = unstable_cache(
   async ({ postcode }: { postcode: string }) => {
+    const searchPostcode = postcode.toUpperCase().replace(/ /g, "");
+
     const result = await db.query.premisesTable.findMany({
-      where: eq(premisesTable.searchPostcode, postcode),
+      where: eq(premisesTable.searchPostcode, searchPostcode),
       columns: {
         createdAt: false,
       },
