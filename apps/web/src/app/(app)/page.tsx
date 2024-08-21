dcc1035aadd8c67d742a0611e1c0f8710852c4c6
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-08-21 18:46:14 +0100

    fix add suspense

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/ui/card";
 import PremisesSearchForm from "@/ui/premises-search-form";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Page = () => {
   return (
@@ -8,7 +8,9 @@ const Page = () => {
       <h1 className="text-3xl font-semibold">Check your bin day</h1>
       <div className="mt-8">
         <Card className="p-8 max-w-3xl">
-          <PremisesSearchForm />
+          <Suspense>
+            <PremisesSearchForm />
+          </Suspense>
         </Card>
       </div>
     </div>
