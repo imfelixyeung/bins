7c61eef26d8154ce3147b98fb30a48626245307b
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-08-21 21:48:29 +0100

    add docs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,15 @@
+import nextra from "nextra";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+  flexsearch: {
+    codeblocks: false,
+  },
+});
+
+export default withNextra(nextConfig);
---
+++ apps/web/theme.config.tsx
@@ -0,0 +1,32 @@
+import type { DocsThemeConfig } from "nextra-theme-docs";
+
+export default {
+  logo: <b>Bins Docs</b>,
+  project: {
+    link: "https://github.com/felixyeungdev/bins",
+    icon: null, // hide github
+  },
+  docsRepositoryBase:
+    "https://github.com/felixyeungdev/bins/tree/main/apps/web",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Bins Docs",
+    };
+  },
+  toc: {
+    backToTop: true,
+  },
+  editLink: {
+    component: null, // hide github
+  },
+  feedback: {
+    content: null, // hide github
+  },
+  footer: {
+    text: (
+      <span>
+        © {new Date().getFullYear()} Felix Yeung. All rights reserved.
+      </span>
+    ),
+  },
+} satisfies DocsThemeConfig;
