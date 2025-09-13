725a4ffa50e2acaf0955f3662695d8ab90e24136
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-14 00:32:36 +0100

    update scripts

--- scripts/database-dump-restore.sh
+++ scripts/database-dump-restore.sh
@@ -1,2 +1,15 @@
-docker exec -it CONTAINER pg_dump -U postgres db > pg_dump.backup
-cat pg_dump.backup | docker exec -i CONTAINER psql -U postgres db
+#!/usr/bin/env bash
+
+# script to dump the contents of a local environment database into a production instance
+# when the prod instance doesn't have enough memory to parse all the csv
+
+mkdir -p temp
+
+echo 'creating ./temp/pg_dump.backup'
+docker exec -it $(docker ps --filter "NAME=bins_db" -q) pg_dump --clean -U postgres db > ./temp/pg_dump.backup
+
+echo 'uploading ./temp/pg_dump.backup to production server'
+rsync -av --progress ./temp/pg_dump.backup bins:~/
+
+echo 'restoring the database'
+ssh bins -t 'cat ~/pg_dump.backup | docker exec -i $(docker ps --filter "NAME=bins_db" -q) psql -U postgres db'
--- scripts/docker-upgrade.sh
+++ scripts/docker-upgrade.sh
@@ -3,3 +3,5 @@
 docker image pull registry.digitalocean.com/felixyeung/bins:web
 docker image pull registry.digitalocean.com/felixyeung/bins:worker
 docker image pull registry.digitalocean.com/felixyeung/bins:migration
+
+docker stack deploy --with-registry-auth -c docker-compose.yaml bins
