b2637b23ef45346069800392f8e7b81e6208a320
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-15 22:46:44 +0100

    use etag checker

--- apps/worker/update/jobs.ts
+++ apps/worker/update/jobs.ts
@@ -1,9 +1,13 @@
 import { execSync } from "node:child_process";
+import etag from "../etag";
+import { urls } from "../data";
 
-export const updateJobs = async () => {
-  try {
-    execSync("./bin/import-csv jobs");
-  } catch (error) {
-    throw error;
-  }
-};
+export const updateJobs = async () =>
+  etag.run(urls.jobs, async () => {
+    try {
+      execSync("./bin/import-csv jobs");
+    } catch (error) {
+      throw error;
+    }
+    return;
+  });
--- apps/worker/update/premises.ts
+++ apps/worker/update/premises.ts
@@ -1,9 +1,13 @@
 import { execSync } from "node:child_process";
+import etag from "../etag";
+import { urls } from "../data";
 
-export const updatePremises = async () => {
-  try {
-    execSync("./bin/import-csv premises");
-  } catch (error) {
-    throw error;
-  }
-};
+export const updatePremises = async () =>
+  etag.run(urls.jobs, async () => {
+    try {
+      execSync("./bin/import-csv premises");
+    } catch (error) {
+      throw error;
+    }
+    return;
+  });
