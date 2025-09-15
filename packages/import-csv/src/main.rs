6938ff080c9ecfbd3e1cd5001b3e6ff3682510fb
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-15 22:00:48 +0100

    use arguments

--- packages/import-csv/src/main.rs
+++ packages/import-csv/src/main.rs
@@ -8,6 +8,22 @@ mod sync;
 async fn main() {
     env_logger::init();
 
+    // Read arguments
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        log::error!("Usage: {} <premises|jobs>", args[0]);
+        return;
+    }
+
+    let task = match args[1].as_str() {
+        "premises" | "jobs" => args[1].as_str(),
+        _ => {
+            log::error!("Unknown argument '{}'. Use 'premises' or 'jobs'", args[1]);
+            return;
+        }
+    };
+
     // connect to database
     let (db, connection) = tokio_postgres::connect(
         "host=localhost user=postgres password=postgres dbname=db port=56432 connect_timeout=5",
@@ -24,67 +40,77 @@ async fn main() {
         }
     });
 
-    let premises = sync::DatabaseSyncPremises {
-        csv_url: "https://opendata.leeds.gov.uk/downloads/bins/dm_premises.csv".to_string(),
-        table_name: "dm_premises".to_string(),
-        nullable_columns: vec![
-            "address_room".to_string(),
-            "address_number".to_string(),
-            "address_street".to_string(),
-            "address_locality".to_string(),
-            "address_city".to_string(),
-            "address_postcode".to_string(),
-        ],
-        csv_columns: vec![
-            "id".to_string(),
-            "address_room".to_string(),
-            "address_number".to_string(),
-            "address_street".to_string(),
-            "address_locality".to_string(),
-            "address_city".to_string(),
-            "address_postcode".to_string(),
-        ],
-        csv_column_schema: vec![
-            Type::INT8,
-            Type::TEXT,
-            Type::TEXT,
-            Type::TEXT,
-            Type::TEXT,
-            Type::TEXT,
-            Type::TEXT,
-        ],
-        copy_columns: vec![
-            "id".to_string(),
-            "address_room".to_string(),
-            "address_number".to_string(),
-            "address_street".to_string(),
-            "address_locality".to_string(),
-            "address_city".to_string(),
-            "address_postcode".to_string(),
-            "search_postcode".to_string(),
-        ],
-    };
+    match task {
+        "premises" => {
+            let premises = sync::DatabaseSyncPremises {
+                csv_url: "https://opendata.leeds.gov.uk/downloads/bins/dm_premises.csv".to_string(),
+                table_name: "dm_premises".to_string(),
+                nullable_columns: vec![
+                    "address_room".to_string(),
+                    "address_number".to_string(),
+                    "address_street".to_string(),
+                    "address_locality".to_string(),
+                    "address_city".to_string(),
+                    "address_postcode".to_string(),
+                ],
+                csv_columns: vec![
+                    "id".to_string(),
+                    "address_room".to_string(),
+                    "address_number".to_string(),
+                    "address_street".to_string(),
+                    "address_locality".to_string(),
+                    "address_city".to_string(),
+                    "address_postcode".to_string(),
+                ],
+                csv_column_schema: vec![
+                    Type::INT8,
+                    Type::TEXT,
+                    Type::TEXT,
+                    Type::TEXT,
+                    Type::TEXT,
+                    Type::TEXT,
+                    Type::TEXT,
+                ],
+                copy_columns: vec![
+                    "id".to_string(),
+                    "address_room".to_string(),
+                    "address_number".to_string(),
+                    "address_street".to_string(),
+                    "address_locality".to_string(),
+                    "address_city".to_string(),
+                    "address_postcode".to_string(),
+                    "search_postcode".to_string(),
+                ],
+            };
 
-    premises.process(&db).await;
+            premises.process(&db).await;
+        }
 
-    let jobs = sync::DatabaseSyncJobs {
-        csv_url: "https://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv".to_string(),
-        table_name: "dm_jobs".to_string(),
-        nullable_columns: vec![],
-        csv_columns: vec![
-            "premises_id".to_string(),
-            "bin".to_string(),
-            "date".to_string(),
-        ],
-        csv_column_schema: vec![Type::INT8, Type::TEXT, Type::DATE],
-        copy_columns: vec![
-            "premises_id".to_string(),
-            "bin".to_string(),
-            "date".to_string(),
-        ],
-    };
+        "jobs" => {
+            let jobs = sync::DatabaseSyncJobs {
+                csv_url: "https://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv".to_string(),
+                table_name: "dm_jobs".to_string(),
+                nullable_columns: vec![],
+                csv_columns: vec![
+                    "premises_id".to_string(),
+                    "bin".to_string(),
+                    "date".to_string(),
+                ],
+                csv_column_schema: vec![Type::INT8, Type::TEXT, Type::DATE],
+                copy_columns: vec![
+                    "premises_id".to_string(),
+                    "bin".to_string(),
+                    "date".to_string(),
+                ],
+            };
 
-    jobs.process(&db).await;
+            jobs.process(&db).await;
+        }
+
+        _ => {
+            log::error!("Unknown argument '{}'. Use 'premises' or 'jobs'", args[1]);
+        }
+    }
 
     log::info!("Done!");
 }
