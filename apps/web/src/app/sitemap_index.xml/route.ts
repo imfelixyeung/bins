205ff9f71325937372de52ad98fb729f5cef4b74
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-18 18:29:49 +0100

    dynamic route, cache sitemap

--- apps/web/src/app/sitemap_index.xml/route.ts
+++ apps/web/src/app/sitemap_index.xml/route.ts
@@ -1,9 +1,9 @@
 import { getPremisesSitemapPages } from "@/lib/sitemap";
 import { NextResponse } from "next/server";
 import { BASE_URL, BUILD_TIME } from "../config";
+import { unstable_cache } from "next/cache";
 
-export const dynamic = "force-static";
-export const revalidate = 60;
+export const dynamic = "force-dynamic";
 
 type Sitemap = { url: string; lastModified?: Date };
 
@@ -24,24 +24,32 @@ const generateSitemapIndex = async (sitemaps: Sitemap[]) => {
   `;
 };
 
+const cachedSitemapIndex = unstable_cache(
+  async () => {
+    const pages = await getPremisesSitemapPages();
+
+    const sitemaps: Sitemap[] = [
+      {
+        url: new URL("/sitemap.xml", BASE_URL).toString(),
+        lastModified: BUILD_TIME,
+      },
+      {
+        url: new URL("/docs/sitemap.xml", BASE_URL).toString(),
+        lastModified: BUILD_TIME,
+      },
+      ...pages.map((page) => ({
+        url: new URL(`/premises/sitemap/${page.id}.xml`, BASE_URL).toString(),
+      })),
+    ];
+
+    return await generateSitemapIndex(sitemaps);
+  },
+  [],
+  { revalidate: 60 }
+);
+
 export const GET = async () => {
-  const pages = await getPremisesSitemapPages();
-
-  const sitemaps: Sitemap[] = [
-    {
-      url: new URL("/sitemap.xml", BASE_URL).toString(),
-      lastModified: BUILD_TIME,
-    },
-    {
-      url: new URL("/docs/sitemap.xml", BASE_URL).toString(),
-      lastModified: BUILD_TIME,
-    },
-    ...pages.map((page) => ({
-      url: new URL(`/premises/sitemap/${page.id}.xml`, BASE_URL).toString(),
-    })),
-  ];
-
-  const xml = await generateSitemapIndex(sitemaps);
+  const xml = await cachedSitemapIndex();
 
   return new NextResponse(xml, {
     headers: {
