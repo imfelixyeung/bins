b5e3475c4a0adb3f2cb88c2590a139a73aa31af6
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-20 16:47:53 +0100

    add check to see if header is defined

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,7 +3,7 @@ import { type NextRequest, NextResponse } from "next/server";
 export const middleware = async (request: NextRequest) => {
   const proxySecret = request.headers.get("X-RapidAPI-Proxy-Secret");
 
-  if (proxySecret !== process.env.RAPID_API_PROXY_SECRET) {
+  if (proxySecret && proxySecret !== process.env.RAPID_API_PROXY_SECRET) {
     return new NextResponse("Bad request", { status: 400 });
   }
 
