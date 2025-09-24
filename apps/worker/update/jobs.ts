d6f6b9f28c744e3192482611e0278e2f0d67f493
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-09-24 23:55:15 +0100

    fix: promisify exec to allow connections

--- apps/worker/update/jobs.ts
+++ apps/worker/update/jobs.ts
@@ -1,11 +1,14 @@
-import { execSync } from "node:child_process";
+import { exec as _exec } from "node:child_process";
 import etag from "../etag";
 import { urls } from "../data";
+import { promisify } from "util";
+
+const exec = promisify(_exec);
 
 export const updateJobs = async () =>
   etag.run(urls.jobs, async () => {
     try {
-      execSync("./bin/import-csv jobs");
+      await exec("./bin/import-csv jobs");
     } catch (error) {
       throw error;
     }
--- apps/worker/update/premises.ts
+++ apps/worker/update/premises.ts
@@ -1,11 +1,14 @@
-import { execSync } from "node:child_process";
+import { exec as _exec } from "node:child_process";
 import etag from "../etag";
 import { urls } from "../data";
+import { promisify } from "util";
+
+const exec = promisify(_exec);
 
 export const updatePremises = async () =>
   etag.run(urls.premises, async () => {
     try {
-      execSync("./bin/import-csv premises");
+      await exec("./bin/import-csv premises");
     } catch (error) {
       throw error;
     }
